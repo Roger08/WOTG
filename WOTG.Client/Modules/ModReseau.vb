8bca1203678181d3903dabc2887e1906582443b2
Roger08 <hugoxerox.tw@gmail.com>
2012-11-01 17:53:12 +0100

    Reception des paquets côté client.

--- WOTG.Client/Modules/ModReseau.vb
+++ WOTG.Client/Modules/ModReseau.vb
@@ -28,4 +28,48 @@ Module ModReseau
     Public Sub InitPaquets()
 
     End Sub
+
+    ' - Deconnecte le client
+    Public Sub Deconnexion()
+        _Socket.Close()
+        _Flux = Nothing
+        _Flux.Dispose()
+    End Sub
+
+    ' - Récéption du paquet
+    Public Sub RecevoirPaquets()
+
+        ' - Variables utilisées pour la récéption + le traitement du paquet
+        Dim PaquetByte() As Byte
+        Dim PaquetString As String
+        Dim Paquet() As String
+        Dim temp() As String
+
+        ' - Boucle de récéption des paquets
+        With _Socket
+            While .Connected
+                If .Poll(10, SelectMode.SelectRead) And .Available = 0 Then
+                    ' Socket serveur semble déconnecté
+                    Call Deconnexion()
+
+                ElseIf .Available > 0 Then
+                    ' - Socket client semble connecté et à envoyé un paquet
+                    ReDim PaquetByte(.Available) ' Défini la taille du paquet
+                    _Flux.Read(PaquetByte, 0, PaquetByte.Length) ' Lecture du paquet
+                    PaquetString = ASCIIEncoding.UTF8.GetString(PaquetByte) ' Convertion du paquet
+                    Paquet = PaquetString.Split(FIN) ' Découpe des paquets possiblement collés
+
+                    ' - Boucle vérifiant que plusieurs paquet ne sont pas collés
+                    For i = 0 To Paquet.Length - 2
+                        ' - Traite le paquet
+                        ' Met le paquet dans la file d'attente
+                        PaquetData.Add(Paquet(i))
+
+                        temp = Paquet(i).Split(SEP) ' Récupère l'entête
+                        PaquetHandler(CByte(temp(0))).Invoke() ' Apelle la fonction correspondante au paquet
+                    Next
+                End If
+            End While
+        End With
+    End Sub
 End Module
